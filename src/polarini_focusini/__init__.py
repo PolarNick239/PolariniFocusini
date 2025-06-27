fc5361bedfdc2569bf59ed3cd4b5dabd05ddfabd
Nikolai Poliarnyi <polarnick239@gmail.com>
2025-06-27 18:44:46 +0300

    v0.1.3 release

--- src/polarini_focusini/__init__.py
+++ src/polarini_focusini/__init__.py
@@ -8,4 +8,4 @@ Usage
 from .infocus_detection import detect_infocus_mask  # re-export  :contentReference[oaicite:1]{index=1}
 
 __all__ = ["detect_infocus_mask"]
-__version__ = "0.1.2"
+__version__ = "0.1.3"
